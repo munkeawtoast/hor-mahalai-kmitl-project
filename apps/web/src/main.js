e7acf89939e6e3fde3fce5e7a17acfc764e391c7
munkeawtoast <munkeawtoast@outlook.com>
2023-03-04 12:41:02 +0700

    feat!(web): add primevue

--- apps/web/src/main.js
+++ apps/web/src/main.js
@@ -8,13 +8,22 @@ import { fas } from '@fortawesome/free-solid-svg-icons'
 import { far } from '@fortawesome/free-regular-svg-icons'
 import { fab } from '@fortawesome/free-brands-svg-icons'
 
+import PrimeVue from 'primevue/config'
+// import Galleria from 'primevue/galleria'
 
 const app = createApp(App)
 library.add(fas, far, fab)
 
 // https://stackoverflow.com/questions/66389974/using-font-awesome-in-vue-3
-// app.component('font-awesome-icon', FontAwesomeIcon)
+app.component('font-awesome-icon', FontAwesomeIcon)
 
+app.use(
+  PrimeVue,
+  {
+    inputStyle: 'outlined',
+    ripple: false 
+  },
+)
 app.use(router)
 
 app.mount('#app')
