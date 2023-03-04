1df06eb1aa942fd19fbae50fd5d8af057d0cd0a9
munkeawtoast <munkeawtoast@outlook.com>
2023-03-05 00:33:05 +0700

    feat(web): add 404 page

--- apps/web/src/router/index.js
+++ apps/web/src/router/index.js
@@ -1,6 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
-import NotFound from '../views/404.vue'
+import NotFound from '../views/404Page.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
