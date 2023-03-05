cdc217d0f50ad5c72c1229573473187286432f7f
munkeawtoast <munkeawtoast@outlook.com>
2023-03-06 00:05:22 +0700

    chore(web): add absolute path

--- apps/web/vite.config.js
+++ apps/web/vite.config.js
@@ -8,7 +8,10 @@ export default defineConfig({
   plugins: [vue()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
+      components: fileURLToPath(new URL('./src/components', import.meta.url)),
+      views: fileURLToPath(new URL('./src/views', import.meta.url)),
+      assets: fileURLToPath(new URL('./src/assets', import.meta.url)),
+      router: fileURLToPath(new URL('./src/router', import.meta.url)),
+    },
+  },
 })
