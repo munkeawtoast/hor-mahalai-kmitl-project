1c5a094dc67906a3e732a5868d7bbdce6a87be2e
munkeawtoast <munkeawtoast@outlook.com>
2023-03-06 15:16:37 +0700

    feat(web): add headlessui

--- apps/web/tailwind.config.js
+++ apps/web/tailwind.config.js
@@ -13,9 +13,17 @@ module.exports = {
       black: '#363636',
       'less-black': '#565656',
       white: colors.white,
-      'less-white': '#f1f1f1',
+      'less-white': '#f4f4f4',
+      'lesser-white': '#f1f1f1',
       gray: '#818181',
       'lesser-gray': '#c6c6c6',
+      danger: '#e74c3c',
+      'soft-danger': '#f5b7b1',
+    },
+    extends: {
+      aspectRatio: {
+        '4/3': '4 / 3',
+      },
     },
   },
   plugins: [],
