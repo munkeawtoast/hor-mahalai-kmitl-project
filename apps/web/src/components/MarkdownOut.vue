2a666bd312faee9b6d889e8d970058be32e80a56
munkeawtoast <munkeawtoast@outlook.com>
2023-03-06 15:47:53 +0700

    ใช้ git cli ไม่เป็น 🤦‍♀️

---
+++ apps/web/src/components/MarkdownOut.vue
@@ -0,0 +1,34 @@
+<script>
+import marked from 'marked'
+
+marked.setOptions({
+  breaks: true,
+  gfm: true,
+  mangle: true,
+  sanitize: true,
+  silent: true,
+  smartypants: true,
+})
+
+export default {
+  props: {
+    content: {
+      type: String,
+      required: true,
+    },
+  },
+  computed: {
+    markdown() {
+      return marked.parse(this.content)
+    },
+  },
+  mounted() {
+    this.markdown = marked.parse(this.content)
+  },
+}
+</script>
+<template>
+  {{ markdown }}
+</template>
+<style>
+</style>
