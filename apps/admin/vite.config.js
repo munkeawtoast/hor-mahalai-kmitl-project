6a31230de8a0d88b02206d6d038e81cd7520379e
munkeawtoast <munkeawtoast@outlook.com>
2023-03-08 18:18:06 +0700

    change port

--- apps/admin/vite.config.js
+++ apps/admin/vite.config.js
@@ -6,12 +6,12 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
-    port: 4000
-  }
+    port: 4000,
+  },
   plugins: [vue()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
 })
