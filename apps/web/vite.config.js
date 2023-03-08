edee2b5ef147c30f906f0a002cc2727a2e63ff5d
munkeawtoast <munkeawtoast@outlook.com>
2023-03-08 18:15:54 +0700

    change port

--- apps/web/vite.config.js
+++ apps/web/vite.config.js
@@ -6,6 +6,9 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  server: {
+    port: 3000,
+  },
   resolve: {
     alias: {
       components: fileURLToPath(new URL('./src/components', import.meta.url)),
