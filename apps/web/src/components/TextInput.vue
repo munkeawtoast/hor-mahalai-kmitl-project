c697040f22d925055e16b7136a37c0876e8f586c
KayKaedesu <t2gtkuhakusora@gmail.com>
2023-04-05 16:53:28 +0700

    lol

---
+++ apps/web/src/components/TextInput.vue
@@ -0,0 +1,29 @@
+<script>
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+export default {
+  props: ['modelValue'],
+  emits: ['update:modelValue'],
+  components: {
+    FontAwesomeIcon,
+  },
+}
+</script>
+<template>
+  <div class="border border-gray bg-white focus-within:border-danger">
+    <span class="border-r border-gray">
+      <FontAwesomeIcon icon="fa-solid fa-user"></FontAwesomeIcon>
+    </span>
+    <input
+      :value="modelValue"
+      @input="
+        ($event) => {
+          $emit('update:modelValue', $event.target.value)
+        }
+      "
+      type="text"
+      placeholder="Username"
+      class="indent-1 focus:border-danger"
+    />
+  </div>
+</template>
+<style></style>
--- apps/web/src/views/RegisterPage.vue
+++ apps/web/src/views/RegisterPage.vue
@@ -1,5 +1,7 @@
 <script>
 import IconLogo from '../components/icons/IconLogo.vue'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import TextInput from '../components/TextInput.vue'
 export default {
   el: '#regis',
   data() {
@@ -18,38 +20,41 @@ export default {
   },
   components: {
     IconLogo,
+    FontAwesomeIcon,
+    TextInput,
   },
   methods: {
     submitLocal() {
-      if (
-        [
-          this.userName,
-          this.password,
-          this.confirmPassword,
-          this.firstName,
-          this.lastName,
-          this.dob,
-          this.email,
-          this.role,
-        ].every((el) => el !== '')
-      ) {
-        let userInfo = {
-          id: 5,
-          userName: this.userName,
-          firstName: this.firstName,
-          lastName: this.lastName,
-          dob: this.dob,
-          email: this.email,
-          userImages: this.images,
-          role: this.role,
-        }
-        let userObject = JSON.stringify(userInfo)
-        localStorage.setItem('userObject', userObject)
+      console.log(this.userName)
+      // if (
+      //   [
+      //     this.userName,
+      //     this.password,
+      //     this.confirmPassword,
+      //     this.firstName,
+      //     this.lastName,
+      //     this.dob,
+      //     this.email,
+      //     this.role,
+      //   ].every((el) => el !== '')
+      // ) {
+      //   let userInfo = {
+      //     id: 5,
+      //     userName: this.userName,
+      //     firstName: this.firstName,
+      //     lastName: this.lastName,
+      //     dob: this.dob,
+      //     email: this.email,
+      //     userImages: this.images,
+      //     role: this.role,
+      //   }
+      //   let userObject = JSON.stringify(userInfo)
+      //   localStorage.setItem('userObject', userObject)
 
-        this.$router.push({ path: '/' })
-      } else {
-        alert('โปรดกรอกข้อมูลให้ครบ')
-      }
+      //   this.$router.push({ path: '/' })
+      // } else {
+      //   alert('โปรดกรอกข้อมูลให้ครบ')
+      // }
     },
     backFunction() {
       this.$router.push({ path: '/' })
@@ -74,31 +79,49 @@ export default {
 }
 </script>
 <template>
-  <div id="regis">
-    <div class="container">
-      <p class="backButton" @click="backFunction">&#60; Go back</p>
-      <IconLogo></IconLogo>
-      <form @submit.prevent="onSubmit">
-        <label>Username: </label>
-        <input v-model="userName" type="text" />
-
+  <div id="regis" class="flex h-screen flex-col items-center justify-center">
+    <p
+      class="absolute top-4 left-4 cursor-pointer hover:text-danger"
+      @click="backFunction"
+    >
+      &#60; Go back
+    </p>
+    <IconLogo></IconLogo>
+    <form class="flex flex-col gap-4 p-8 text-lg" @submit.prevent="onSubmit">
+      <p>Register</p>
+      <!-- <div class="border border-gray bg-white">
+        <span class="border-r border-gray">
+          <FontAwesomeIcon icon="fa-solid fa-user"></FontAwesomeIcon>
+        </span>
+        <input
+          type="text"
+          v-model="userName"
+          placeholder="Username"
+          class="indent-1"
+        />
+      </div> -->
+      <TextInput v-model="userName"></TextInput>
+      <!-- <div>
         <label>Password: </label>
-        <input v-model="password" type="password" /><br />
+        <input v-model="password" type="password" />
         <label>Confirm password: </label>
         <input v-model="confirmPassword" type="password" />
-        <span class="danger">{{ confirmPasswordText }}</span
-        ><br />
-
+        <span class="danger">{{ confirmPasswordText }}</span>
+      </div>
+      <div>
         <label>First name: </label>
         <input v-model="firstName" type="text" />
         <label>Last name: </label>
-        <input v-model="lastName" type="text" /><br />
-
+        <input v-model="lastName" type="text" />
+      </div>
+      <div>
         <label>Date of birth: </label>
-        <input v-model="dob" type="date" /><br />
+        <input v-model="dob" type="date" />
         <label>Email: </label>
         <input v-model="email" type="text" />
-        <label>Images</label><br />
+      </div>
+      <div>
+        <label>Images</label>
         <label>Role</label>
         <input
           v-model="role"
@@ -110,13 +133,15 @@ export default {
         <label>User</label>
         <input v-model="role" type="radio" name="userType" value="dormOwner" />
         <label>Dorm owner</label><br />
-        <button @click="submitLocal">Register</button>
-      </form>
-    </div>
+      </div> -->
+      <button class="w-48 rounded-md bg-lesser-gray p-2" @click="submitLocal">
+        Register
+      </button>
+    </form>
   </div>
 </template>
 <style scoped>
-form {
+/* form {
   font-size: 1.5em;
   font-weight: bold;
 }
@@ -129,7 +154,7 @@ button {
 /* a{
   all:
 } */
-.container {
+/* .container {
   display: flex;
   justify-content: center;
   align-items: center;
@@ -151,5 +176,5 @@ button {
 
 .danger {
   color: red;
-}
+} */
 </style>
