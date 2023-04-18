8cdbdc019db910911f76098e3324ad1ea016dae7
munkeawtoast <munkeawtoast@outlook.com>
2023-04-18 16:29:34 +0700

    feat(api): stuff

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -2,18 +2,16 @@ import express from 'express'
 import homeRoute from './routes/home.js'
 import dormRoute from './routes/dorm.js'
 import ticketRoute from './routes/ticket.js'
-import { checkAuth } from 'middlewares/auth.js'
+// import { checkAuth } from 'middlewares/auth.js'
 import { expressjwt as jwt } from 'express-jwt'
+import dotenv from 'dotenv'
+
+dotenv.config()
 
 const app = express()
 
-const PORT = process.env.PORT || 3000
-app.use(
-  jwt({
-    secret: process.env.JWT_SECRET,
-    algorithms: ['HS256'],
-  }),
-)
+const PORT = process.env.API_PORT
+
 
 app.use('/', homeRoute)
 // app.use('/dorms', dormRoute)
--- apps/api/src/routes/ticket.ts
+++ apps/api/src/routes/ticket.ts
@@ -1,12 +1,10 @@
 import { Router } from 'express'
 import { Request } from 'util/types.js'
-import { checkAuth } from '../middlewares/auth.js'
+// import { checkAuth } from '../middlewares/auth.js'
 
 const ticketRoute = Router()
 
 // query Tickets, must be moderator
-ticketRoute.get('/', checkAuth, (req: Request, res) => {
-
-})
+ticketRoute.get('/', (req: Request, res) => {})
 
 export default ticketRoute
