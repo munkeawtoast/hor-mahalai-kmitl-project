0a002bf5c4cc21eba551fdaa5106f89e2e39c512
munkeawtoast <munkeawtoast@outlook.com>
2023-05-13 22:04:50 +0700

    merge

--- shared/validator/src/users/index.ts
+++ shared/validator/src/users/index.ts
@@ -1,1 +1,17 @@
+import { z } from 'zod'
 export {}
+export const zLogin = () =>
+  z.object({
+    username: z.string().max(30).or(z.string().email()),
+    password: z.string().regex(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).*$/, 'dsfsdfs'),
+  })
+export const zRegister = () =>
+  z.object({
+    username: z.string().max(30),
+    password: z.string().regex(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).*$/, 'dsfsdfs'),
+    firstname: z.string().max(50),
+    lastname: z
+      .string()
+      .max(50)
+      .regex(/^[\p{L}เ\s'-]+$/),
+  })
