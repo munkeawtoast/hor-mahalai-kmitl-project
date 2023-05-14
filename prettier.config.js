db3964c56333eb2e26ed1a0548f0e1e016e12777
munkeawtoast <munkeawtoast@outlook.com>
2023-05-14 15:54:49 +0700

    config

--- prettier.config.js
+++ prettier.config.js
@@ -1,6 +1,10 @@
 module.exports = {
-  "singleQuote": true,
-  "trailingComma": "all",
-  "semi": false,
-  "tabWidth": 2
+  singleQuote: true,
+  trailingComma: 'all',
+  semi: false,
+  tabWidth: 2,
+  endOfLine: 'lf',
+  vueIndentScriptAndStyle: false,
+  arrowParens: 'avoid',
+  bracketSameLine: false,
 }
