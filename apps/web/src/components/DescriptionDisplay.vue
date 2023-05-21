c32d691ce499892256ce03186a47ff0872a182e3
munkeawtoast <munkeawtoast@outlook.com>
2023-05-21 16:28:04 +0700

    dorm description display

---
+++ apps/web/src/components/DescriptionDisplay.vue
@@ -0,0 +1,32 @@
+<script setup>
+import { defineProps } from 'vue'
+import Typography from '@tiptap/extension-typography'
+import { Editor, EditorContent, StarterKit } from '@tiptap/vue-3'
+
+const props = defineProps({
+  modelValue: {
+    default: '',
+    type: String,
+  },
+})
+
+const editor = new Editor({
+  content: props.modelValue,
+  extensions: [
+    StarterKit.configure({
+      code: false,
+      codeBlock: false,
+    }),
+    Typography,
+  ],
+  editorProps: {
+    attributes: {
+      class:
+        'prose sm:prose-xs xl:prose-base max-w-[100%] min-h-[120px] focus:outline-none',
+    },
+  },
+})
+</script>
+<template>
+  <EditorContent :editor="editor" />
+</template>
