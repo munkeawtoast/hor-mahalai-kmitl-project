145b472b5c7c135a198f7147c1be4d752bfe2580
munkeawtoast <munkeawtoast@outlook.com>
2023-05-21 16:05:49 +0700

    remove getdormbylandmark

--- apps/api/src/router/web/index.ts
+++ apps/api/src/router/web/index.ts
@@ -1,6 +1,5 @@
 import { Router } from 'express'
 import dormRouter from './dorms.js'
-import landmarkRouter from './landmarks.js'
 import universityRouter from './universities.js'
 import userRounter from './users.js'
 import cors from 'cors'
@@ -10,7 +9,6 @@ const webRouter = Router()
 
 webRouter.use(cors())
 webRouter.use('/dorms', dormRouter)
-webRouter.use('/landmarks', landmarkRouter)
 webRouter.use('/universities', universityRouter)
 webRouter.use('/users', userRounter)
 webRouter.use('/tickets', ticketsRounter)
